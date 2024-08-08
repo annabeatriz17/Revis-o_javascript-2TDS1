1ed6ad7333a76c96e031ffb91e8f8e60efa77a7c
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-08-08 10:59:32 -0300

    mudanças

--- batalhaderobos.js
+++ batalhaderobos.js
@@ -1,16 +1,17 @@
 let armas = ["Pistola","Revolver","Rifle", "Fuzil","Laser"];
 let ataques = [];
 
-ataques.push(armas[0]);
+ataques.push("Eva");
 ataques.push(armas[1]);
 ataques.push(armas[2]);
-ataques.push(armas[3]);
 ataques.push(armas[4]);
+ataques.push(armas[0]);
+
 
 for (let i = 0; i < ataques.length; i++) {
-    if (ataques[0] == armas[1] ) {
-        console.log(Wall-E)
-
+    if (i == 0) {
+        console.log(`Nome do Robô: ${ataques[i]}`);
+    }else {
+        console.log(`Ataque ${i}: ${ataques[i]}`);
     }
-        console.log(ataques[0]=pistola);
 }
